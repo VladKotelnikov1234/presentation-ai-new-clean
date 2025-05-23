d472b9024456ab0f4b31cff8f3a4e975bd1a50db
VladKotelnikov1234 <kotelnikov322@gmail.com>
2025-05-23 19:47:35 +0500

    Fix DJANGO_SETTINGS_MODULE path in wsgi.py to backend.backend.settings

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,8 +1,16 @@
-import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+"""
+WSGI config for backend project.
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)  
